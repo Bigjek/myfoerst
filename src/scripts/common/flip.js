adf29a66083ab1accf71c7d99fbe1c32d0e1b954
Evgeniy Kocheshkov <ivapira@mail.ru>
2017-10-10 12:44:37 +0300

    add change

--- src/scripts/common/flip.js
+++ src/scripts/common/flip.js
@@ -1,15 +1,18 @@
-function flipInit(){
-  $('#auth').on('click', function(e){
-    e.preventDefault();
-    $(this).addClass('btn-hidden');
-    $('.flipper').toggleClass('active');
-  });
-  $('#mainIndex').on('click', function(e){
-    e.preventDefault();
-    $('#auth').removeClass('btn-hidden');
-    $('.flipper').removeClass('active');
-  });
-
-}
+const flipInit = (function() {
+  return {
+    init: function() {
+      $('#auth').on('click', function(e){
+        e.preventDefault();
+        $(this).addClass('btn-hidden');
+        $('.flipper').toggleClass('active');
+      });
+      $('#mainIndex').on('click', function(e){
+        e.preventDefault();
+        $('#auth').removeClass('btn-hidden');
+        $('.flipper').removeClass('active');
+      });
+    },
+  };
+}());
   
 module.exports = flipInit;
--- src/scripts/common/scroll-down.js
+++ src/scripts/common/scroll-down.js
@@ -1,11 +1,14 @@
-function scrollDownInit(){
-  $(document).on('click', '.arrow-down__btn',function(e){
-    e.preventDefault();
-    var scrollB = $(this).data('href');
-    $('.wrapper').stop().animate({
-      scrollTop: $(scrollB).offset().top,
-    }, 1500);
-  });
-}
+const scrollDownInit = (function() {
+  return {
+    init: function() {
+      $('.arrow-down__btn').on('click', function(){
+        var scrollB = $(this).data('href');
+        $('.wrapper').stop().animate({
+          scrollTop: $(scrollB).offset().top + 115,
+        }, 1500);
+      });
+    },
+  };
+}());
 
 module.exports = scrollDownInit;
